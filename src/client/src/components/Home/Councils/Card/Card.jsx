2bd6ac19876a10f75de2b9dc6739626d0f96e8d5
Shreya Verma <112201007@smail.iitpkd.ac.in>
2024-03-10 12:09:30 +0530

    1

--- src/client/src/components/Home/Councils/Card/Card.jsx
+++ src/client/src/components/Home/Councils/Card/Card.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
+
 
 const Card = (props) => {
+
+  const handleClick = () => {
+    if (props.linkUrl) {
+      window.history.pushState(null, null, window.location.href);
+      window.location.href = props.linkUrl;
+    } else {
+      console.error("linkUrl is undefined");
+    }
+  };
+
   return (
-    <div className="card crd" style={{width:'22rem',height:'20rem',margin:'2rem 1.5rem 0rem'}}>
-      <img src={props.imgUrl} class="card-img-top crdimg" alt="..." style={{width:'22rem',height:'16rem'}} />
-      <div class="card-body crdtit">
-        <p class="card-text" style={{textAlign:'center',fontSize:'150%'}}>{props.title}</p>
+    <div className="card crd" style={{ width: '22rem', height: '20rem', margin: '2rem 1.5rem 0rem' }} onClick={handleClick}>
+      <img src={props.imgUrl} className="card-img-top crdimg" alt="..." style={{ width: '22rem', height: '16rem' }} />
+      <div className="card-body crdtit">
+        <p className="card-text" style={{ textAlign: 'center', fontSize: '150%' }}>{props.title}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/client/src/components/Home/Councils/Councils.jsx
+++ src/client/src/components/Home/Councils/Councils.jsx
@@ -6,26 +6,33 @@ const Councils = (props) => {
     const arr = [
     {
       'Title' : 'Tech Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/technical',
+      'imgUrl' :'techn.jpeg'
+      
+    },
+    {
+      'Title' : 'Hostel Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/hostel',
+      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+   },
+    {
+      'Title' : 'Cultural Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/cultural',
       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
     },
     {
-      'Title' : 'Host Council',
+      'Title' : 'Sports Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/sports',
       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
     },
     {
       'Title' : 'Acad Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
     },
     {
       'Title' : 'Acad Council',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-    {
-      'Title' : 'Acad Council',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-    {
-      'Title' : 'Acad Council',
+      'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
     },
   ]
@@ -35,7 +42,7 @@ const Councils = (props) => {
     <div className='cards d-flex flex-row flex-wrap justify-content-center align-items-center'>
       {
         arr.map((obj,i)=>(
-            <Card imgUrl = {obj.imgUrl} title = {obj.Title}/>
+            <Card imgUrl = {obj.imgUrl} title = {obj.Title} linkUrl = {obj.linkUrl}/>
         ))
       }
     </div>
