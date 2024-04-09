d2c48dedf15813317e99a55c1ebcff7eeac1fd50
Shreya Verma <112201007@smail.iitpkd.ac.in>
2024-04-10 01:49:53 +0530

    data_from_mock_server

--- src/client/src/components/Dashboard/More/StaticData/StaticData.jsx
+++ src/client/src/components/Dashboard/More/StaticData/StaticData.jsx
@@ -1,92 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import LayoutLeft from '../../Layout/LayoutLeft/LayoutLeft';
+import LayoutRight from '../../Layout/LayoutRight/LayoutRight';
+import StaticDataComponent from './StaticDataComponent';
 
-// import React, { useState, useEffect } from 'react';
-// import LayoutLeft from '../../Layout/LayoutLeft/LayoutLeft';
-// import LayoutRight from '../../Layout/LayoutRight/LayoutRight';
-// import StaticDataComponent from './StaticDataComponent';
+const StaticData = () => {
+  const [items, setItems] = useState([]);
 
-// const StaticData = () => {
-//   const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/items');
+      const data = await response.json();
+      setItems(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-//   const fetchData = async () => {
-//     try {
-//       const response = await fetch('http://localhost:8000/items');
-//       const data = await response.json();
-//       setItems(data);
-//     } catch (error) {
-//       console.error('Error fetching data:', error);
-//     }
-//   };
+  return (
+    <div className='d-flex flex-row'>
+      <div style={{ width: '20rem' }}>
+        <LayoutLeft ele='statdat' />
+      </div>
+      <div className='d-flex justify-content-center' style={{ width: 'calc(100% - 43rem)' }}>
+        <div className='' style={{ width: '50rem', margin: '3rem 1rem', textAlign: 'justify' }}>
+          <div className='staticDataHead' style={{ marginLeft: '5rem' }}>
+            <h1>Static Data</h1>
+          </div>
+          <div className="staticDataContainer container">
+            {items.map(item => (
+              <div className="anncCont mt-5" key={item.heading}>
+                <StaticDataComponent heading={item.heading} overview={item.overview} link={item.link} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div style={{ width: '20rem' }}>
+        <LayoutRight />
+      </div>
+    </div>
+  );
+}
 
-//   return (
-//     <div className='d-flex flex-row'>
-//       <div style={{ width: '20rem' }}>
-//         <LayoutLeft ele='statdat' />
-//       </div>
-//       <div className='d-flex justify-content-center' style={{ width: 'calc(100% - 43rem)' }}>
-//         <div className='' style={{ width: '50rem', margin: '3rem 1rem', textAlign: 'justify' }}>
-//           <div className='staticDataHead' style={{ marginLeft: '5rem' }}>
-//             <h1>Static Data</h1>
-//           </div>
-//           <div className="staticDataContainer container">
-//             {items.map(item => (
-//               <div className="anncCont mt-5" key={item.heading}>
-//                 <StaticDataComponent heading={item.heading} overview={item.overview} link={item.link} />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//       <div style={{ width: '20rem' }}>
-//         <LayoutRight />
-//       </div>
-//     </div>
-//   );
-// }
+export default StaticData;
 
-// export default StaticData;
-
-
-
-
-
-// import React from 'react';
-// import LayoutLeft from '../../Layout/LayoutLeft/LayoutLeft';
-// import LayoutRight from '../../Layout/LayoutRight/LayoutRight';
-// import StaticDataComponent from './StaticDataComponent';
-// import StaticDataData from '../../../../data/static_data.json';
-
-// const StaticData = () => {
-//   const { items } = StaticDataData;
-
-//   return (
-//     <div className='d-flex flex-row'>
-//       <div style={{ width: '20rem' }}>
-//         <LayoutLeft ele='statdat' />
-//       </div>
-//       <div className='d-flex justify-content-center' style={{width:'calc(100% - 43rem)'}}>
-//       <div className='' style={{ width: '50rem', margin: '3rem 1rem', textAlign: 'justify'}}>
-//         <div className='staticDataHead' style={{marginLeft: '5rem'}}>
-//           <h1>Static Data</h1>
-//         </div>
-//         <div className="staticDataContainer container">
-//           {items.map(item => (
-//             <div className="anncCont mt-5" key={item.heading}>
-//               <StaticDataComponent heading={item.heading} overview={item.overview} link={item.link} />
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//       </div>
-//       <div style={{ width: '20rem' }}>
-//         <LayoutRight />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default StaticData;
 
--- src/client/src/components/Home/Councils/Councils.jsx
+++ src/client/src/components/Home/Councils/Councils.jsx
@@ -1,53 +1,90 @@
-import React from 'react'
-import Card from './Card/Card'
-import './Councils.css'
+import React, { useState, useEffect } from 'react';
+import Card from './Card/Card';
+import './Councils.css';
 
-const Councils = (props) => {
-    const arr = [
-    {
-      'Title' : 'Tech Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/technical',
-      'imgUrl' :'techn.jpeg'
-      
-    },
-    {
-      'Title' : 'Hostel Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/hostel',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-   },
-    {
-      'Title' : 'Cultural Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/cultural',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-    {
-      'Title' : 'Sports Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/sports',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-    {
-      'Title' : 'Acad Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-    {
-      'Title' : 'Acad Council',
-      'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
-      'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
-    },
-  ]
+const Councils = () => {
+  const [councils, setCouncils] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/councils');
+      const data = await response.json();
+      setCouncils(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div id='homeCouncils' className='container councils d-flex flex-column justify-content-center align-items-center mt-5 mb-5'>
-    <h1 className='conHead position-relative d-inline-block pb-4'>Councils</h1>
-    <div className='cards d-flex flex-row flex-wrap justify-content-center align-items-center'>
-      {
-        arr.map((obj,i)=>(
-            <Card imgUrl = {obj.imgUrl} title = {obj.Title} linkUrl = {obj.linkUrl}/>
-        ))
-      }
+      <h1 className='conHead position-relative d-inline-block pb-4'>Councils</h1>
+      <div className='cards d-flex flex-row flex-wrap justify-content-center align-items-center'>
+        {councils.map((council) => (
+          <Card key={council.id} imgUrl={council.imgUrl} title={council.title} linkUrl={council.linkUrl} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Councils
+export default Councils;
+
+
+
+// import React from 'react'
+// import Card from './Card/Card'
+// import './Councils.css'
+
+// const Councils = (props) => {
+//     const arr = [
+//     {
+//       'Title' : 'Tech Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/technical',
+//       'imgUrl' :'techn.jpeg'
+      
+//     },
+//     {
+//       'Title' : 'Hostel Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/hostel',
+//       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+//    },
+//     {
+//       'Title' : 'Cultural Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/cultural',
+//       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+//     },
+//     {
+//       'Title' : 'Sports Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/sports',
+//       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+//     },
+//     {
+//       'Title' : 'Acad Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
+//       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+//     },
+//     {
+//       'Title' : 'Acad Council',
+//       'linkUrl': 'https://sac.iitpkd.ac.in:8090/academic',
+//       'imgUrl' : 'https://imgs.search.brave.com/BMuYABP7oP4l8HymmSOQIH30nF_YQMtJm-y7Bz-vc6Q/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9idXJz/dC5zaG9waWZ5Y2Ru/LmNvbS9waG90b3Mv/dHdvLXRvbmUtaW5r/LWNsb3VkLmpwZz93/aWR0aD0xMDAwJmZv/cm1hdD1wanBnJmV4/aWY9MCZpcHRjPTA'
+//     },
+//   ]
+//   return (
+//     <div id='homeCouncils' className='container councils d-flex flex-column justify-content-center align-items-center mt-5 mb-5'>
+//     <h1 className='conHead position-relative d-inline-block pb-4'>Councils</h1>
+//     <div className='cards d-flex flex-row flex-wrap justify-content-center align-items-center'>
+//       {
+//         arr.map((obj,i)=>(
+//             <Card imgUrl = {obj.imgUrl} title = {obj.Title} linkUrl = {obj.linkUrl}/>
+//         ))
+//       }
+//     </div>
+//     </div>
+//   )
+// }
+
+// export default Councils
