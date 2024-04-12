b19fc4074700b2316837ab277d75dd68dd7a6a82
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-04-12 15:05:42 +0530

    mock json data added and started being fetched

--- src/client/src/components/Dashboard/More/Budget/RecentTrans/RecentTrans.jsx
+++ src/client/src/components/Dashboard/More/Budget/RecentTrans/RecentTrans.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const RecentTrans = (props) => {
-    const colHead = ['Name', 'Mode', 'Date', 'Amount']
-    const objs = [
+    const [colHead, setColHead] = useState(['Name', 'Mode', 'Date', 'Amount'])
+    const [objs, setObjs] = useState([
         {
             Name: 'Figma',
             Mode: 'Cash',
@@ -21,26 +22,36 @@ const RecentTrans = (props) => {
             Date: '26-06-2023',
             Amount: 1850
         },
-    ]
+    ])
+
+    useEffect(()=>{
+        axios.get('http://localhost:8000/Recent')
+        .then(res => {
+            setColHead(res.data.colHead)
+            setObjs(res.data.objs)
+        })
+        .catch(er => console.log(er))
+    },[])
+
     return (
-        <div className='d-flex flex-column' style={{padding:'1rem 0rem',border:'2px dashed white',borderRadius:'1rem'}}>
-            <div className='d-flex align-items-center' style={{padding:'0rem 3rem',borderBottom:'2px dashed black'}}>
-                <p className='' style={{fontWeight:'600',fontSize:'115%'}}>Recent Transactions</p>
+        <div className='d-flex flex-column' style={{ padding: '1rem 0rem', border: '2px dashed white', borderRadius: '1rem' }}>
+            <div className='d-flex align-items-center' style={{ padding: '0rem 3rem', borderBottom: '2px dashed black' }}>
+                <p className='' style={{ fontWeight: '600', fontSize: '115%' }}>Recent Transactions</p>
             </div>
             <div className='d-flex flex-row justify-content-between align-items-center pt-2'>
                 {colHead.map((data) => (
-                    <div className='d-flex justify-content-center' style={{width:'10rem'}}>
-                        <p style={{fontWeight:'bold',color:'gray'}}>{data}</p>
+                    <div className='d-flex justify-content-center' style={{ width: '10rem' }}>
+                        <p style={{ fontWeight: 'bold', color: 'gray' }}>{data}</p>
                     </div>
                 ))}
             </div>
             {objs.map((obj) => (
                 <div className='d-flex flex-row justify-content-between align-items-center'>
                     {colHead.map((data) => (
-                        <div className='d-flex justify-content-center' style={{width:'10rem'}}>
-                        {
-                            data == 'Amount' ? obj[data] > 0 ? <p style={{fontWeight:'bold',color:'green'}}>{obj[data]} <span>👎🏿</span></p> : <p style={{fontWeight:'bold',color:'rgb(224, 64, 251)'}}>{obj[data]} ₹</p> : <p style={{fontWeight:'bold'}}>{obj[data]}</p>
-                        }
+                        <div className='d-flex justify-content-center' style={{ width: '10rem' }}>
+                            {
+                                data == 'Amount' ? obj[data] > 0 ? <p style={{ fontWeight: 'bold', color: 'green' }}>{obj[data]} <span>👎🏿</span></p> : <p style={{ fontWeight: 'bold', color: 'rgb(224, 64, 251)' }}>{obj[data]} ₹</p> : <p style={{ fontWeight: 'bold' }}>{obj[data]}</p>
+                            }
                         </div>
                     ))}
                 </div>
